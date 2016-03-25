d4930bafc454a9a569ada850cf2079ea6c250200
jahantech <jahansyed@hotmail.co.uk>
2016-03-25 18:39:26 +0000

    python function to write to a log file, can be handy for scripts

---
+++ write_to_log_file.py
@@ -0,0 +1,14 @@
+
+
+from time import gmtime, strftime
+
+
+
+def WriteToLogFile(LogEntry):
+    filehandler = open("python_log.txt", 'a')
+    filehandler.write(strftime("%Y-%m-%d %H:%M:%S", gmtime()) +" : "+ LogEntry+"\n")
+    filehandler.close()
+    return
+
+
+WriteToLogFile("User logged in") 
